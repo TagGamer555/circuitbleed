f230d3f5a04832dfef6fc799aa7c14c80dc733bc
Alexander <69044271+TagGamer555@users.noreply.github.com>
2025-11-24 18:11:36 +0100

    Refactor thermal socket and plug configurations; update descriptions, adjust efficiency values, and increment version to build 5. Add new images for active and inactive thermal sockets.

--- scripts/yttria/blocks_environment.js
+++ scripts/yttria/blocks_environment.js
@@ -1,5 +1,13 @@
 {
     // yttria/blocks_environment.js
+    const thermal_socket = Attribute.add("thermal_socket");
+    
     const thermal_socket_active   = extend(Floor, "thermal-socket-active", {});
     const thermal_socket_inactive = extend(Floor, "thermal-socket-inactive", {});
+
+    thermal_socket_active.attributes.set(thermal_socket, 1.0);
+
+    module.exports = {
+        thermal_socket: thermal_socket
+    }
 }
--- scripts/yttria/blocks_power.js
+++ scripts/yttria/blocks_power.js
@@ -1,4 +1,8 @@
 {
     // yttria/blocks_power.js
-    const thermal_plug = extend(ThermalGenerator, "thermal-plug", {});
+    const env = require("yttria/blocks_environment.js"); // I'm not sure if this is the right way to do it...
+
+    const thermal_plug = extend(ThermalGenerator, "thermal-plug", {
+        attribute: env.thermal_socket
+    });
 }
