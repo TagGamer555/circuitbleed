c3d4f5ff870dcaa3575e367dbd822d34effd2ba5
Alexander <69044271+TagGamer555@users.noreply.github.com>
2025-11-24 18:23:45 +0100

    Update version to build 7, restore thermal socket configurations, and correct require path in blocks_power.js

--- scripts/yttria/blocks_power.js
+++ scripts/yttria/blocks_power.js
@@ -1,8 +1,6 @@
 {
     // yttria/blocks_power.js
-    const env = require("blocks_environment.js"); // attempt 2
-
     const thermal_plug = extend(ThermalGenerator, "thermal-plug", {
-        attribute: env.thermal_socket
+        attribute: thermal_socket
     });
 }
