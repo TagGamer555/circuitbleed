1e33fa15071e5afba19062aa7cbb2a7b716d904c
Alexander <69044271+TagGamer555@users.noreply.github.com>
2025-11-24 18:14:38 +0100

    Update version to build 6 in mod.hjson and correct require path in blocks_power.js

--- scripts/yttria/blocks_power.js
+++ scripts/yttria/blocks_power.js
@@ -1,6 +1,6 @@
 {
     // yttria/blocks_power.js
-    const env = require("yttria/blocks_environment.js"); // I'm not sure if this is the right way to do it...
+    const env = require("blocks_environment.js"); // attempt 2
 
     const thermal_plug = extend(ThermalGenerator, "thermal-plug", {
         attribute: env.thermal_socket
